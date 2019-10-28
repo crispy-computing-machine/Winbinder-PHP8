c29012e1a63202e40077d1c2e575bc043da1d821
Stefan <stefan.brittain@ogl.co.uk>
2019-10-28 14:27:54 +0000

    reverted to previous INI loading - leave for a later date to debug

--- phpwb_export.c
+++ phpwb_export.c
@@ -23,16 +23,20 @@
 
 
 //----------------------------------------------- PROTOTYPES FOR THE ZEND ENGINE
-ZEND_BEGIN_MODULE_GLOBALS(winbinder)
-    zend_ulong debug_level;
-ZEND_END_MODULE_GLOBALS(winbinder)
-ZEND_DECLARE_MODULE_GLOBALS(winbinder)
+// @todo debug why this doesnt appear to work :(
+//ZEND_BEGIN_MODULE_GLOBALS(winbinder)
+//    zend_ulong debug_level;
+//ZEND_END_MODULE_GLOBALS(winbinder)
+//ZEND_DECLARE_MODULE_GLOBALS(winbinder)
+//PHP_INI_BEGIN()
+    // Default validators from PHP are OnUpdateLongGEZero(), OnUpdateLong(), OnUpdateBool(), OnUpdateReal(), OnUpdateString(), and OnUpdateStringUnempty().
+    // OnUpdateLongGEZero: default validator that exists in PHP and validates the value against a long greater than or equal to zero
+    // STD_PHP_INI_ENTRY("winbinder.debug_level", "0", PHP_INI_ALL, OnUpdateLong, debug_level, zend_winbinder_globals, winbinder_globals)
+//PHP_INI_END()
 
 // ---------------------------------------------------------- INI SETTINGS
-// OnUpdateLongGEZero: default validator that exists in PHP and validates the value against a long greater than or equal to zero
-// Default validators from PHP are OnUpdateLongGEZero(), OnUpdateLong(), OnUpdateBool(), OnUpdateReal(), OnUpdateString(), and OnUpdateStringUnempty().
 PHP_INI_BEGIN()
-    STD_PHP_INI_ENTRY("winbinder.debug_level", "0", PHP_INI_ALL, OnUpdateLong, debug_level, zend_winbinder_globals, winbinder_globals)
+PHP_INI_ENTRY("winbinder.debug_level", "0", PHP_INI_ALL, NULL)
 PHP_INI_END()
 
 ZEND_MINIT_FUNCTION(winbinder);
