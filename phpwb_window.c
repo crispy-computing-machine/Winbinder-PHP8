7108a5a8699382c506256d13647eb949ae5df94d
Stefan <stefan.brittain@ogl.co.uk>
2019-10-29 10:52:28 +0000

    handler error logging

--- phpwb_window.c
+++ phpwb_window.c
@@ -395,7 +395,7 @@ ZEND_FUNCTION(wb_set_handler)
 		//zend_error(E_WARNING, "%s(): '%s' is not a function or cannot be called", get_active_function_name(TSRMLS_C), fname);
 		//wbError(TEXT(__FUNCTION__), E_WARNING, TEXT("%s is not a function or cannot be called"), fname);
 
-        php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s is not a function or cannot be called", &fname);
+        php_error_docref(NULL TSRMLS_CC, E_WARNING, "%s is not a function or cannot be called", &handler);
 
 		RETURN_NULL();
 	} else {
