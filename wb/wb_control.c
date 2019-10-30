2ac1620ec957e4c68aef43d5c5341cb891381fe1
Stefan <stefan.brittain@ogl.co.uk>
2019-10-30 14:38:54 +0000

    default font pwbo value

--- wb/wb_control.c
+++ wb/wb_control.c
@@ -105,7 +105,7 @@ PWBOBJ wbCreateControl(PWBOBJ pwboParent, UINT uWinBinderClass, LPCTSTR pszSourc
 	pwbo->pszCallBackObj = NULL;
 	pwbo->lparam = lParam;
 	ZeroMemory(pwbo->lparams, sizeof(LONG) * 8);
-	pwbobj->lparams[7] = 0; // default font
+	pwbo->lparams[7] = 0; // default font
 	ZeroMemory(&pwbo->rcTitle, sizeof(RECT) + 2 * sizeof(AREA));
 	pwbo->pbuffer = NULL;
 
