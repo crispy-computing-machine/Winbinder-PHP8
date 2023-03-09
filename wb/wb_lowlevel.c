3271cfdb7d9917561b0fed9469e4bd9ac54618bd
Crispy Computing Machine <5684066+crispy-computing-machine@users.noreply.github.com>
2023-03-09 11:49:08 +0000

    wbLoadLibrary debug

--- wb/wb_lowlevel.c
+++ wb/wb_lowlevel.c
@@ -54,17 +54,17 @@ HMODULE wbLoadLibrary(LPCTSTR pszLibName)
 	if (!hLib)
 	{
 		wsprintf(szName, TEXT("%s32"), pszLibName);
-		printf("wbLoadLibrary: 1: %ls\n", szName);
+		printf("wbLoadLibrary: 1: %s \n", szName);
 		hLib = LoadLibrary(szName);
 		if (!hLib)
 		{
 			wsprintf(szName, TEXT("%s.exe"), pszLibName);
-			printf("wbLoadLibrary: 2: %ls\n", szName);
+			printf("wbLoadLibrary: 2: %s \n", szName);
 			hLib = LoadLibrary(szName);
 			if (!hLib)
 			{
 				wsprintf(szName, TEXT("%s32.exe"), pszLibName);
-				printf("wbLoadLibrary: 3: %ls\n", szName);
+				printf("wbLoadLibrary: 3: %s \n", szName);
 				hLib = LoadLibrary(szName);
 			}
 		}
