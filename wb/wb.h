3e37ffe664d623ee167db4ba60d9ebf4b6b6687d
Stefan <author@email.com>
2023-04-03 12:18:47 +0100

    wbGetBitmapBits func param fixes

--- wb/wb.h
+++ wb/wb.h
@@ -442,7 +442,7 @@ BOOL wbCopyPartialBits(HDC hdc, HBITMAP hBitmap, int xTarget, int yTarget, int n
 BOOL wbMaskPartialBits(HDC hdc, HBITMAP hbmImage, HBITMAP hbmMask, int xTarget, int yTarget, int nWidth, int nHeight, int xSource, int ySource);
 //HANDLE		wbLoadImage(LPCTSTR pszImageFile, UINT64 nIndex);
 HANDLE wbLoadImage(LPCTSTR pszImageFile, UINT64 nIndex, LPARAM lParam);
-DWORD64 wbGetBitmapBits(HBITMAP hbm, BYTE **lpBits, BOOL bCompress4to3);
+DWORD64 wbGetBitmapBits(HBITMAP hbm);
 unsigned char* readHBitmap(HBITMAP hBitmap, int* width, int* height);
 COLORREF wbGetPixelDirect(unsigned char *pixdata, int xPos, int yPos, BOOL bCompress4to3);
 
