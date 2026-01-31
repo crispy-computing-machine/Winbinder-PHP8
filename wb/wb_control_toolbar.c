b5467e0f38b48d9bcb1fca4fae0581d50987f0cd
brittain <yup@yup.com>
2026-01-31 11:32:46 +0000

    fix toolbar tooltips

--- wb/wb_control_toolbar.c
+++ wb/wb_control_toolbar.c
@@ -131,7 +131,8 @@ static BOOL CreateToolbarButton(HWND hwnd, int id, int nIndex, LPCTSTR pszHint)
 		if (pszHint && *pszHint)
 		{
 		// fix encoding? tbb.dwData = (DWORD_PTR)Utf82WideChar(pszHint);
-			tbb.dwData = (DWORD_PTR)_wcsdup(pszHint);
+		    tbb.dwData = (DWORD_PTR)Utf82WideChar((const char*)pszHint);
+			//tbb.dwData = (DWORD_PTR)_wcsdup(pszHint);
 		}
 		else
 			tbb.dwData = 0;
