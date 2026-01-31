326338c589007ab94318112f42c06b71482542b6
brittain <yup@yup.com>
2026-01-31 15:08:11 +0000

    Revert "fix toolbar tooltips"
    This reverts commit e37f8022e2875ac71f72754f74f31add3a10d3f3.

--- wb/wb_control_toolbar.c
+++ wb/wb_control_toolbar.c
@@ -130,9 +130,8 @@ static BOOL CreateToolbarButton(HWND hwnd, int id, int nIndex, LPCTSTR pszHint)
 		tbb.fsStyle = TBSTYLE_BUTTON;
 		if (pszHint && *pszHint)
 		{
-		    // fix encoding? tbb.dwData = (DWORD_PTR)Utf82WideChar(pszHint);
-		    //tbb.dwData = (DWORD_PTR)Utf82WideChar((const char*)pszHint);
-		    tbb.dwData = (DWORD_PTR)Utf82WideChar((const char*)pszHint, 0);
+		// fix encoding? tbb.dwData = (DWORD_PTR)Utf82WideChar(pszHint);
+		    tbb.dwData = (DWORD_PTR)Utf82WideChar((const char*)pszHint);
 			//tbb.dwData = (DWORD_PTR)_wcsdup(pszHint);
 		}
 		else
